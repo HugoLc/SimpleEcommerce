28bf2f5aefeae9fdaee4df37025d60af7723e76f
Hugo <hugo@m3ecommerce.com>
2024-03-21 22:12:29 -0300

    feat: publish app

--- obj/Debug/net8.0/SimpleEcommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3cf789bb4110b749cc3f8437eaa5f40038538c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c75072e03c6b7959c5e1885ea0653421d1e67d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
